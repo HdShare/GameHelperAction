f23a91ae0f8f9ae03b62f72043d466c470a59de5
hd <1839732296@qq.com>
2024-06-04 15:42:47 +0800

    style(main)

--- src/main.py
+++ src/main.py
@@ -3,9 +3,13 @@ import sys
 
 if __name__ == '__main__':
     sys.path.append(os.getcwd())
-    # from src.env import EnvUtil
-    # EnvUtil.init()
+    from src.env import EnvUtil
     from src.pg import PgEntry
     from src.smoba import SmobaEntry
+
+    # 本地配置
+    # EnvUtil.init()
+    # 和平营地
     PgEntry.entry()
+    # 王者营地
     SmobaEntry.entry()
