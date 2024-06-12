a090880df3625d21acbf221839dfa298b19b626d
hd <1839732296@qq.com>
2024-06-13 00:25:22 +0800

    style(project)

--- src/pg/PgEntry.py
+++ src/pg/PgEntry.py
@@ -12,16 +12,14 @@ def signin():
     global send_content
     resp_json = HttpApi.signin()
     if resp_json is not None:
-        send_content += ">签到成功"
+        send_content += ">签到成功\n"
     else:
-        send_content += ">签到失败"
+        send_content += ">签到失败\n"
 
 
 def do_task_list():
-    global send_content
     resp_json = HttpApi.task_list()
     if resp_json is not None:
-        send_content += ">获取任务列表成功"
         task_list = resp_json["data"]["taskList"]
         for task in task_list:
             task_id = task["taskId"]
@@ -31,18 +29,16 @@ def do_task_list():
                 task_complete(task_id, task_detail)
             elif task_status == 2:
                 HttpApi.task_complete(task_id)
-    else:
-        send_content += ">获取任务列表失败"
 
 
 def task_complete(task_id, task_detail):
     global send_content
     if task_id == 26:
         # print("本日通过和平营地启动1次游戏")
-        send_content += f">任务失败(无法完成): {task_id}-{task_detail}"
+        send_content += ">任务失败: 无法启动游戏\n"
     elif task_id == 27:
         # print("本日取胜1局计分模式（前五）")
-        send_content += f">任务失败(无法完成): {task_id}-{task_detail}"
+        send_content += ">任务失败: 无法取胜前五\n"
     elif task_id == 28:
         # print("本日为资讯点赞3次")
         page_info = HttpApi.page_info()
@@ -52,12 +48,12 @@ def task_complete(task_id, task_detail):
                 if HttpApi.like_info(page_info["data"]["list"][i]["iInfoId"], "1") is not None:
                     like_count += 1
             if like_count == 3:
-                send_content += f">任务成功: {task_id}-{task_detail}"
+                send_content += ">任务成功: 点赞资讯完成\n"
                 HttpApi.task_complete(task_id)
             else:
-                send_content += f">任务失败(点赞异常): {task_id}-{task_detail}"
+                send_content += ">任务失败: 点赞资讯出错\n"
         else:
-            send_content += f">任务失败(资讯异常): {task_id}-{task_detail}"
+            send_content += ">任务失败: 获取资讯出错\n"
     elif task_id == 29:
         # print("本日为动态点赞3次")
         page_moment = HttpApi.page_moment()
@@ -67,12 +63,12 @@ def task_complete(task_id, task_detail):
                 if HttpApi.like_moment(page_moment["data"]["list"][i]["momentId"], "1") is not None:
                     like_count += 1
             if like_count == 3:
-                send_content += f">任务成功: {task_id}-{task_detail}"
+                send_content += ">任务成功: 点赞动态完成\n"
                 HttpApi.task_complete(task_id)
             else:
-                send_content += f">任务失败(点赞异常): {task_id}-{task_detail}"
+                send_content += ">任务失败: 点赞动态出错\n"
         else:
-            send_content += f">任务失败(动态异常): {task_id}-{task_detail}"
+            send_content += ">任务失败: 获取动态出错\n"
     elif task_id == 30:
         # print("本日浏览资讯5分钟")
         ext_data = [{
@@ -96,24 +92,24 @@ def task_complete(task_id, task_detail):
     elif task_id == 33:
         # print("本周启用游戏工具至少一个")
         if HttpApi.game_tool("4", "1") is not None:
-            send_content += f">任务成功: {task_id}-{task_detail}"
+            send_content += ">任务成功: 启用工具完成\n"
             HttpApi.task_complete(task_id)
         else:
-            send_content += f">任务失败(启用异常): {task_id}-{task_detail}"
+            send_content += ">任务失败: 启用工具出错\n"
     elif task_id == 34:
         # print("本日分享资讯到社交网络")
         if HttpApi.share("shareInfo", "1") is not None:
-            send_content += f">任务成功: {task_id}-{task_detail}"
+            send_content += ">任务成功: 分享资讯完成\n"
             HttpApi.task_complete(task_id)
         else:
-            send_content += f">任务失败(分享异常): {task_id}-{task_detail}"
+            send_content += ">任务失败: 分享资讯出错\n"
     elif task_id == 35:
         # print("本周分享战绩周报到社交网络")
         if HttpApi.share("shareH5", "https://c.gp.qq.com/camp/weekly/index") is not None:
-            send_content += f">任务成功: {task_id}-{task_detail}"
+            send_content += ">任务成功: 分享周报完成\n"
             HttpApi.task_complete(task_id)
         else:
-            send_content += f">任务失败(分享异常): {task_id}-{task_detail}"
+            send_content += ">任务失败: 分享周报出错\n"
     elif task_id == 36:
         # print("观看PEL赛事直播5分钟")
         ext_data = [{
@@ -125,7 +121,7 @@ def task_complete(task_id, task_detail):
         }] * 12
         module_report(ext_data, task_id, task_detail)
     else:
-        send_content += f">未知任务(无法完成): {task_id}-{task_detail}"
+        send_content += f">未知任务: {task_id}-{task_detail}\n"
 
 
 def module_report(ext_data, task_id, task_detail):
@@ -135,17 +131,15 @@ def module_report(ext_data, task_id, task_detail):
         if HttpApi.module_report(ext_data) is not None:
             report_count += 1
     if report_count == 5:
-        send_content += f">任务成功: {task_id}-{task_detail}"
+        send_content += f">任务成功: {task_detail}\n"
         HttpApi.task_complete(task_id)
     else:
-        send_content += f">任务失败(观看异常): {task_id}-{task_detail}"
+        send_content += f">任务失败: {task_detail}出错\n"
 
 
 def do_gift_list():
-    global send_content
     resp_json = HttpApi.task_list()
     if resp_json is not None:
-        send_content += ">获取礼包列表成功"
         gift_list = resp_json["data"]["liveness"]["livenessGiftList"]
         for gift in gift_list:
             gift_id = gift["giftId"]
@@ -153,8 +147,6 @@ def do_gift_list():
             gift_status = gift["status"]
             if gift_status == 1:
                 gift_receive(gift_id, gift_title)
-    else:
-        send_content += ">获取礼包列表失败"
 
 
 def gift_receive(gift_id, gift_title):
@@ -162,17 +154,17 @@ def gift_receive(gift_id, gift_title):
     if gift_id == 1:
         # print("福利币*100")
         if HttpApi.gift_receive(gift_id) is not None:
-            send_content += f">已领取礼包: {gift_id}-{gift_title}"
+            send_content += f">领取成功: {gift_title}\n"
     elif gift_id == 2:
         # print("营地装饰碎片*50")
         if HttpApi.gift_receive(gift_id) is not None:
-            send_content += f">已领取礼包: {gift_id}-{gift_title}"
+            send_content += f">领取成功: {gift_title}\n"
     elif gift_id == 3:
         # print("福利币*200")
         if HttpApi.gift_receive(gift_id) is not None:
-            send_content += f">已领取礼包: {gift_id}-{gift_title}"
+            send_content += f">领取成功: {gift_title}\n"
     else:
-        send_content += f">未知礼包(无法领取): {gift_id}-{gift_title}"
+        send_content += f">未知礼包: {gift_id}-{gift_title}\n"
 
 
 def do_like_records():
@@ -189,8 +181,8 @@ def do_like_records():
 def entry():
     if os.environ.get("pg_enable") == "true":
         global send_content
-        send_content += "#########################################################"
-        send_content += f"# 和平营地 # {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+        send_content += "#####################################\n"
+        send_content += f"# 和平营地 # {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"
         if Util.check_repo_secrets(
                 [
                     "pg_appid",
@@ -203,13 +195,12 @@ def entry():
                     "pg_token"
                 ]
         ):
-            send_content += ">环境变量已配置"
             signin()
             do_task_list()
             do_gift_list()
             do_like_records()
         else:
-            send_content += ">环境变量未配置"
-        send_content += "#########################################################"
+            send_content += ">环境变量未配置\n"
+        send_content += "#####################################\n"
         print(send_content)
-        PushPlus.send("和平营地", send_content)
+        PushPlus.send("GameHelperAction-和平营地", send_content)
--- src/smoba/SmobaEntry.py
+++ src/smoba/SmobaEntry.py
@@ -9,7 +9,6 @@ send_content = ""
 
 
 def do_task_list():
-    global send_content
     resp_json = HttpApi.task_list()
     if resp_json is not None:
         task_list = resp_json["data"]["taskList"]
@@ -30,18 +29,18 @@ def task_complete(task_id, task_desc):
             i = -1
             if list_info_moment["data"]["list"][i]["showType"] == 0:
                 if HttpApi.like_info(list_info_moment["data"]["list"][i]["infoContent"]["infoId"], "1") is not None:
-                    send_content += ">任务成功: 点赞资讯完成"
+                    send_content += ">任务成功: 点赞资讯完成\n"
                 else:
-                    send_content += ">任务失败: 点赞资讯出错"
+                    send_content += ">任务失败: 点赞资讯出错\n"
             elif list_info_moment["data"]["list"][i]["showType"] == 1:
                 if HttpApi.like_moment(list_info_moment["data"]["list"][i]["momentId"], True) is not None:
-                    send_content += ">任务成功: 点赞动态完成"
+                    send_content += ">任务成功: 点赞动态完成\n"
                 else:
-                    send_content += ">任务失败: 点赞动态出错"
+                    send_content += ">任务失败: 点赞动态出错\n"
             else:
-                send_content += ">任务失败: 未知列表类型"
+                send_content += ">任务失败: 未知列表类型\n"
         else:
-            send_content += ">任务失败: 获取列表出错"
+            send_content += ">任务失败: 获取列表出错\n"
     elif task_id == "2024010800001":
         # print("当日浏览营地1篇资讯")
         list_info_moment = HttpApi.list_info_moment()
@@ -53,25 +52,25 @@ def task_complete(task_id, task_desc):
                         is_success = True
                         break
             if is_success:
-                send_content += ">任务成功: 浏览资讯完成"
+                send_content += ">任务成功: 浏览资讯完成\n"
             else:
-                send_content += ">任务失败: 浏览资讯出错"
+                send_content += ">任务失败: 浏览资讯出错\n"
         else:
-            send_content += ">任务失败: 获取列表出错"
+            send_content += ">任务失败: 获取列表出错\n"
     elif task_id == "2024010800002":
         # print("限时任务：前往任意游戏专区签到1次")
         if HttpApi.signin() is not None:
-            send_content += ">任务成功: 专区签到完成"
+            send_content += ">任务成功: 专区签到完成\n"
         else:
-            send_content += ">任务失败: 专区签到出错"
+            send_content += ">任务失败: 专区签到出错\n"
     elif task_id == "2024010800004":
         # print("分享王者营地任一内容到社交网络")
         if HttpApi.play_task_data("1") is not None:
-            send_content += ">任务成功: 分享内容完成"
+            send_content += ">任务成功: 分享内容完成\n"
         else:
-            send_content += ">任务失败: 分享内容出错"
+            send_content += ">任务失败: 分享内容出错\n"
     else:
-        send_content += f">未知任务: {task_id}-{task_desc}"
+        send_content += f">未知任务: {task_id}-{task_desc}\n"
 
 
 def do_task_reward():
@@ -88,9 +87,9 @@ def do_task_reward():
                 task_ids.append(task_id)
         if len(task_ids) > 0:
             if HttpApi.task_reward(task_ids) is not None:
-                send_content += f">领取成功: {len(task_ids)} / {len(task_list)}"
+                send_content += f">领取成功: {len(task_ids)} / {len(task_list)}\n"
             else:
-                send_content += f">领取失败: {len(task_ids)} / {len(task_list)}"
+                send_content += f">领取失败: {len(task_ids)} / {len(task_list)}\n"
 
 
 def do_camp_list():
@@ -109,17 +108,17 @@ def camp_complete(task_id, task_desc):
     global send_content
     if task_id == 1001145:
         # print("今日前往每日福利签到")
-        send_content += ">任务失败: 福利签到异常"
+        send_content += ">任务失败: 福利签到异常\n"
     elif task_id == 1001159:
         # print("每日在任意圈子签到")
         if HttpApi.bbs_sign(25) is not None:
-            send_content += ">任务成功: 圈子签到完成"
+            send_content += ">任务成功: 圈子签到完成\n"
         else:
-            send_content += ">任务失败: 圈子签到出错"
+            send_content += ">任务失败: 圈子签到出错\n"
     elif task_id == 1001295:
-        send_content += "玩家每日观赛30s"
+        send_content += "玩家每日观赛30s\n"
     else:
-        send_content += f">未知任务: {task_id}-{task_desc}"
+        send_content += f">未知任务: {task_id}-{task_desc}\n"
 
 
 def do_camp_reward():
@@ -131,14 +130,14 @@ def do_camp_reward():
         num_finished = group_data["finishedtasknum"]
         if num_finished > 0:
             if HttpApi.camp_task_reward() is not None:
-                send_content += f">领取成功: {num_finished} / {num_task}"
+                send_content += f">领取成功: {num_finished} / {num_task}\n"
 
 
 def entry():
     if os.environ.get("smoba_enable") == "true":
         global send_content
-        send_content += "#########################################################"
-        send_content += f"# 王者营地 # {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+        send_content += "#####################################\n"
+        send_content += f"# 王者营地 # {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"
         if Util.check_repo_secrets(
                 [
                     "smoba_roleId",
@@ -146,11 +145,10 @@ def entry():
                     "smoba_token"
                 ]
         ):
-            send_content += ">环境变量已配置"
             do_task_list()
             do_task_reward()
         else:
-            send_content += ">环境变量未配置"
+            send_content += ">环境变量未配置\n"
         if Util.check_repo_secrets(
                 [
                     "smoba_sMSDKUrlParam",
@@ -158,11 +156,10 @@ def entry():
                     "smoba_sCampUserId"
                 ]
         ):
-            send_content += ">环境变量已配置"
             do_camp_list()
             do_camp_reward()
         else:
-            send_content += ">环境变量未配置"
-        send_content += "#########################################################"
+            send_content += ">环境变量未配置\n"
+        send_content += "#####################################\n"
         print(send_content)
-        PushPlus.send("王者营地", send_content)
+        PushPlus.send("GameHelperAction-王者营地", send_content)
