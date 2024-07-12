cf5c80d45f2d033752ef0d262b2f7f06422e00d0
hd <1839732296@qq.com>
2024-07-12 18:22:32 +0800

    fix(push): ex

--- src/util/PushPlus.py
+++ src/util/PushPlus.py
@@ -1,18 +1,23 @@
 import json
 import os
+import traceback
 
 import requests
 
 
 def send(title, content):
-    requests.post(
-        "https://www.pushplus.plus/send",
-        data=json.dumps(
-            {
-                "token": os.environ.get("pushplus_token"),
-                "title": title,
-                "content": content
-            }
-        ).encode(encoding="utf-8"),
-        headers={"Content-Type": "application/json"}
-    )
+    try:
+        requests.post(
+            "https://www.pushplus.plus/send",
+            data=json.dumps(
+                {
+                    "token": os.environ.get("pushplus_token"),
+                    "title": title,
+                    "content": content
+                }
+            ).encode(encoding="utf-8"),
+            headers={"Content-Type": "application/json"}
+        )
+    except Exception as e:
+        print(f"推送{title}异常: {e}")
+        traceback.print_exc()
